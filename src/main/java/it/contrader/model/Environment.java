adbf414f3598eea9d111a67f6808c2f60145cb44
Marcoale2000 <marcoalemanno52@gmail.com>
2019-11-12 15:12:15 +0100

    creazione classe environment

---
+++ src/main/java/it/contrader/model/Environment.java
@@ -0,0 +1,55 @@
+package it.contrader.model;
+
+	public class Environment {
+		
+		private int id;
+		
+		private String nome;
+
+
+
+		public Environment() {
+		}	
+
+		public Environment(String nome) {
+			this.nome=nome;
+		}
+		public Environment(int id,String nome) {
+			this.id=id;
+			this.nome=nome;
+		}
+		
+		public int getId() {
+			return this.id;
+		}
+		public void setId(int id) {
+			this.id=id;
+		}
+		public void setnome(String nome) {
+			this.nome=nome;
+		}
+		public String getnome() {
+			return nome;
+		}
+		public String toString() {
+			return id + "\t" + nome + "\t\t";
+		}
+		public boolean equals(Object obj) {
+			if(this ==obj)
+				return true;
+			if(obj ==null)
+				return false;
+			if(getClass() !=obj.getClass() )
+				return false;
+			Environment other = (Environment) obj;
+			if(id != other.id)
+				return false;
+			if(nome==null) {
+				if(other.nome !=null)
+					return false;
+			}else if(!nome.contentEquals(other.nome))
+				return false;
+			return true;
+		}
+		
+	}
